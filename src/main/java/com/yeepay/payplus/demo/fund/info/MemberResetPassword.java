1fd34b7f456591061588464b88dc60f82af55f28
Marco Jan <yang.yang-1@yeepay.com>
2016-10-31 15:11:12 +0800

    updated logic of RESET and MODIFY password and added comments

--- src/main/java/com/yeepay/payplus/demo/fund/info/MemberResetPassword.java
+++ src/main/java/com/yeepay/payplus/demo/fund/info/MemberResetPassword.java
@@ -7,6 +7,7 @@ import com.yeepay.payplus.demo.Config;
 
 /**
  * 会员重置支付密码
+ *
  * @author：lei.fang
  * @since：2016年09月02日 15:58
  * @version:1.0
@@ -15,7 +16,7 @@ public class MemberResetPassword {
 
     public static final String INTERFACE_URI = "/rest/v1.0/user/getPswdResetUrl";
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
         YopRequest request = new YopRequest(Config.APP_KEY, Config.APP_SECRET, Config.URL);
         request.setEncrypt(true);
@@ -26,12 +27,14 @@ public class MemberResetPassword {
         request.addParam("requestNo", System.currentTimeMillis());//商户请求号，投产时请不要使用时间戳，避免并发重复
         request.addParam("merchantNo", Config.MERCHANTNO);//商户编号
         request.addParam("merchantUserId", Config.USER_ID_MARCOJAN);//商户用户标识
-        request.addParam("passwordBizType", "MODIFY");//商户用户标识
+        request.addParam("passwordBizType", "RESET");//商户用户标识, 这个参数后续版本可能会删掉
+        request.addParam("clientSource", "PC"); //不传此参数的话默认为MOBILE
         request.addParam("webCallBackUrl", "http://www.yeepay.com/");//页面回调地址,修改密码完成后返回商户的地址
-        request.addParam("returnUrl", "http://www.yeepay.com/");//返回地址,点击 '返回'的回退地址
+        request.addParam("returnUrl", "http://www.qq.com/");//返回地址,点击 '返回'的回退地址
 
         //发送报文
         YopResponse response = YopClient.post(INTERFACE_URI, request);
+
         System.out.println(response.toString());//处理返回结果
 
     }
