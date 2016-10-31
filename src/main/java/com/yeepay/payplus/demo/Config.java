c7634120ef49142fdeb588891c8b883db447b37c
Marco Jan <yang.yang-1@yeepay.com>
2016-10-31 10:16:52 +0800

    removed test info

--- src/main/java/com/yeepay/payplus/demo/Config.java
+++ src/main/java/com/yeepay/payplus/demo/Config.java
@@ -3,22 +3,21 @@ package com.yeepay.payplus.demo;
 public class Config {
     /**
      * 通信URL
+     * 请联系钱麦客服申请入网: ppsupport@yeepay.com
      */
-    public static final String URL = "https://open.yeepay.com/yop-center";
+    public static final String URL = "";
 
     /**
      * 通信商编
+     * 请联系钱麦客服申请入网: ppsupport@yeepay.com
      */
-    public static final String APP_KEY = "BM12345678901221";
-
-    // default BM12345678901221
+    public static final String APP_KEY = "";
 
     /**
-     * 通信密钥å
+     * 通信密钥
+     * 请联系钱麦客服申请入网: ppsupport@yeepay.com
      */
-    public static final String APP_SECRET = "+D8LD8PwkWBFUxk/dsh/Zw==";
-
-    // 默认 +D8LD8PwkWBFUxk/dsh/Zw==
+    public static final String APP_SECRET = "";
 
     /**
      * 加密算法
@@ -27,21 +26,22 @@ public class Config {
 
     /**
      * 测试商户编号
+     * 请联系钱麦客服申请入网: ppsupport@yeepay.com
      */
-    public static final String MERCHANTNO = "BM12345678901221";
+    public static final String MERCHANTNO = "";
 
     /**
      * 测试商户用户标识
      */
-    public static final String MERCHANT_USER_ID = "2354448349";
+    public static final String MERCHANT_USER_ID = "";
 
     /**
      * 测试商户用户标识
      */
-    public static final String USER_ID_MARCOJAN = "marcojan11";
+    public static final String USER_ID_MARCOJAN = "";
 
     /**
      * 测试商户用户标识
      */
-    public static final String TO_USER = "2354448349";
+    public static final String TO_USER = "";
 }
